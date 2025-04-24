42b288f0cfb885ae62fbe60300f4c6379af56ed2
anacprieto <aprieto@e-city.co>
2025-04-24 08:37:31 -0500

    Modelo dto de reporte

---
+++ src/Dto/reportDto.js
@@ -0,0 +1,25 @@
+const reportDto = {
+    id: 0,
+    document: null,
+    documenttype: null,
+    name: null,
+    lastName: null,
+    nacionality: null,
+    birthday: null,
+    phone: null,
+    email: null,
+    adress: null,
+    departament: null,
+    municipality: null,
+    state: 0,
+    idtransaction: 0,
+    idClient: 0,
+    client: null,
+    idUserCreated: 0,
+    userCreated: null,
+    idUserUpdated: 0,
+    userUpdated: null,
+    dateCreated: null
+  };
+  
+  export default reportDto;
